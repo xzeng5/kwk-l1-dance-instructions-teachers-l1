20108656a025e77a67583b4156b500a92788fa8f
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-06-12 22:41:49 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -4,7 +4,7 @@ def starting_stance
   puts "Plant legs far apart, bend knees slightly and keep posture loose"
 end
 
-def skip_step_left
+def skip_step("left")
   puts "Lower left foot to the ground"
   puts "Bounce left foot back up slightly, kicking it a few inches back"
   puts "Left, Right, Left and Left"
