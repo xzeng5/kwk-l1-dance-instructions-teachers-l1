65f881916612671fcb8af69f7f34cc75ca8e0b89
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-06-12 23:05:09 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -22,4 +22,5 @@ def bob_the_reins
   puts "Holding your arms out in front of you, straight and at chest level"
   puts "Cross your right wrist over your left and hold them together"
   puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song"
+  puts "Do this movement eight times"
 end
